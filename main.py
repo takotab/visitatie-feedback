43beee3e076e3df6203d27a6850d0550c6fa52db
tako_desktop <takotako@gmail.com>
2019-03-01 16:25:29 +0100

    .

---
+++ main.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+import logging
+
+
+parser = argparse.ArgumentParser(description="Handeling the results of the visitatie.")
+
+parser.add_argument(
+    "--map",
+    default="data_fake",
+    type=str,
+    help="Select the map to be used for making the results.",
+)
+args = parser.parse_args()
+
+
+def main(map):
+    # TODO get basic info {"naam","naam code","Aantal Therapeuten","bezoekende therapeut code","bezoekende prakijk}
+    # TODO check door de juiste bezocht
+    # TODO get parktijk toets result
+    # TODO extract patient x as dict
+    # TODO 2 meetinstumenten
+    # TODO STartBack/GPE
+    # TODO Dossiertoets
+    # TODO Check num of patienten vs expected
+    # TODO Save praktijk_dict as json
+    # TODO make pdf rapport
+    pass
+
+
+if __name__ == "__main__":
+    main(args.map)
