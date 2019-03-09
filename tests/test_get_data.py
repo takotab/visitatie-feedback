5ad51e685bd1dd256d98722f0c5aad2c2db940b8
tako_desktop <takotako@gmail.com>
2019-03-09 16:42:01 +0100

    .

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -3,5 +3,6 @@ import visitatie
 
 def test_get_data():
     a_form = visitatie.get_data(i=0)
-    a_form.naam = "Fysiotherapie FC Barcalona"
-    assert True
+    assert a_form.naam == "Fysiotherapie Real Madrid"
+    assert a_form.bezoekende_therapeut == "Fysiotherapie FC Barcalona"
+    
--- visitatie/form.py
+++ visitatie/form.py
@@ -5,27 +5,46 @@ class Form(object):
     def __init__(self, pd_serie, path):
         self.path = path
         self.dct = dict(pd_serie)
-        # TODO get basic info {"naam","naam code","Aantal Therapeuten","bezoekende therapeut code","bezoekende prakijk}
+        # TODO get basic info {"Aantal Therapeuten"}
         self.get_basic_info()
 
     def get_basic_info(self):
         self.praktijk_code = self.dct["Praktijkcode van de praktijk die bezocht wordt"]
-        inschrijving_gegevens = self.find_inschrijving_code()
+        inschrijving_gegevens = self.find_inschrijving_info_code()
         self.naam = inschrijving_gegevens["naam"]
         self.email = inschrijving_gegevens["email"]
+        self.aantal_therapeuten = None
         self.bezoekende_therapeut_code = self.dct["Therapeutcode bezoekende therapeut?"]
+        self.bezoekende_therapeut = self.find_inschrijving_info_code(
+            code=str(self.bezoekende_therapeut_code)[:-2]
+        )["naam"]
+        
 
-    def find_inschrijving_code(self, code=None):
+    def find_inschrijving_info_code(self, code=None):
         if code is None:
             code = self.praktijk_code
-        with open(os.path.join(self.path, "gegevens.csv")) as f:
-            for line in f:
-                splited_line = line.replace("\n", "").split(",")
-                if len(splited_line) != 10:
-                    splited_line[7] = ",".join(splited_line[7:9])
-                    splited_line[8:] = splited_line[9:]
 
-                assert len(splited_line) == 10, splited_line
-                if splited_line[5] == str(code):
-                    return {"naam": splited_line[7], "email": splited_line[9]}
+        for line in lines_from_csv_file(os.path.join(self.path, "gegevens.csv")):
+            splited_line = fix_length(line.split(","))
+
+            if splited_line[5] == str(code):
+                return {"naam": splited_line[7], "email": splited_line[9]}
+
         raise FileNotFoundError(code)
+
+
+def lines_from_csv_file(file):
+    with open(file) as f:
+        for line in f:
+            try:
+                yield line.replace("\n", "")
+            except GeneratorExit:
+                pass
+
+
+def fix_length(str_lst: [str]):
+    if len(str_lst) != 10:
+        str_lst[7] = ",".join(str_lst[7:9])
+        str_lst[8:] = str_lst[9:]
+    assert len(str_lst) == 10
+    return str_lst
