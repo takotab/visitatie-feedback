03f2f40c5eea9db0592eb609d317b7f12614ffca
tako_desktop <takotako@gmail.com>
2019-03-12 14:47:06 +0100

    get_patients.tested.

--- tests/test_get_patient.py
+++ tests/test_get_patient.py
@@ -1,7 +1,22 @@
+import os
 import visitatie
 
 
 def test_get_patient():
+    test_f = "visitatie/patient_questions_pytest.txt"
+    if os.path.exists(test_f):
+        os.remove(test_f)
+    visitatie.get_patients.patient_question_file = test_f
+
     a_form = visitatie.get_data(i=0)
-    patients_data = visitatie.get_patients(a_form)
-    print(patients_data)
+    patient_data = visitatie.get_patient_data(a_form, 0)
+    a_form.dct[
+        "Therapeutcode van de behandelde therapeut betreffende dit dossier "
+    ] = 00
+    assert visitatie.get_patient_data(a_form, 0) == patient_data
+
+    patient_data = visitatie.get_patients(a_form)
+    assert set(list(patient_data.keys())) == set([0, 1, "last_patient"])
+    assert patient_data["last_patient"] == 1
+
+    os.remove(test_f)
--- visitatie/get_patients.py
+++ visitatie/get_patients.py
@@ -1,6 +1,7 @@
+import os
 from visitatie.form import Form
 
-PATIENTS_Q = None
+patient_question_file = "visitatie/patient_questions.txt"
 
 
 def get_patients(form: Form):
@@ -17,7 +18,7 @@ def get_patient_data(form: Form, i: int = 0, is_last: bool = False):
     if i in form.patients and not is_last:
         return form.patients[i]
 
-    patient_questions = get_patient_questions(PATIENTS_Q, form)
+    patient_questions = get_patient_questions(form)
     check_is_last(form, i)
 
     form.patients[i] = form.get_keys(
@@ -30,13 +31,14 @@ def get_patient_data(form: Form, i: int = 0, is_last: bool = False):
     return form.patients[i]
 
 
-def get_patient_questions(patient_questions: list, form: Form):
-    if patient_questions is None:
-        all_keys_w_one = [key for key in form.get_keys() if ".1" in key[-2:]]
-        PATIENTS_Q = [key[:-2] for key in all_keys_w_one]
-        return PATIENTS_Q
+def get_patient_questions(form: Form):
+    if not os.path.exists(patient_question_file):
+        return make_patient_question_file(form)
     else:
-        return PATIENTS_Q
+        patient_questions = getattr(form, "patient_questions", None)
+        if patient_questions is None:
+            patient_questions = read_patient_question_file()
+        return patient_questions
 
 
 def check_is_last(form: Form, i: int):
@@ -57,3 +59,21 @@ def check_this_is_last(form: Form, i: int):
 
 def make_addition(i: int):
     return "." + str(i) if i > 0 else ""
+
+
+def make_patient_question_file(form: Form):
+    all_keys_w_one = [key for key in form.get_keys() if ".1" in key[-2:]]
+    patient_questions = [key[:-2] for key in all_keys_w_one]
+    with open(patient_question_file, "w") as f:
+        for q in patient_questions:
+            f.write(q + "\n")
+    return patient_questions
+
+
+def read_patient_question_file():
+    result = []
+    with open(patient_question_file, "r") as f:
+        for line in f:
+            result.append(line.replace("\n", ""))
+    return result
+
