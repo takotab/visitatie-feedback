b5edf88658e6620f5b32b1426ed6025885fe92a9
tako_desktop <takotako@gmail.com>
2019-03-14 20:55:35 +0100

    added dossiertoets. tested.

--- tests/test_dossiertoets.py
+++ tests/test_dossiertoets.py
@@ -5,5 +5,5 @@ def test_dossier_toets():
     a_form = visitatie.get_data(i=0)
     _ = visitatie.get_data_from_all_patients(a_form)
     _dossiertoets = visitatie._dossier_toets(a_form.patients[0])
-    assert _dossiertoets == 1
+    assert _dossiertoets == {"Dossiertoets": 0.25}
 
--- visitatie/dossier_toets.py
+++ visitatie/dossier_toets.py
@@ -1,4 +1,5 @@
 from visitatie.form import Form
+from visitatie.mean import Mean
 
 
 def get_dossier_toets(form: Form):
@@ -8,5 +9,26 @@ def get_dossier_toets(form: Form):
     return result
 
 
-def _dossier_toets(answers: dict, i=0):
-    return 1
+dossier_toets_q = [
+    "Zijn de verwijsgegevens aanwezig in het dossier?",
+    "Is de conclusie van de screening aanwezig?",
+    "Is de hulpvraag van de patiënt vastgelegd?",
+    "Zijn alle domeinen van de ICF beoordeeld binnen het diagnostisch proces?",
+    "Is de oefen/ fysiotherapeutische diagnose aanwezig?",
+    "Is er een behandeldoel vastgelegd? (concreet, meetbaar, eventueel SMART)",
+    "Is het behandelplan opgesteld?    ",
+    "Is er in het behandelplan rekening gehouden met de uitslag van de STarT Back Screening Tool? ",
+    "Voldoet het dossier aan de minimale-klinimetrie eisen van 2 volledig ingevulde meetinstrumenten naast de STarT Back ?",
+    "Wordt er op systematische wijze gebruik gemaakt van klinimetrie?",
+    "Wordt er geëvalueerd op de gestelde behandeldoelen en indien nodig bijgesteld?",
+    "Is er, kijkend naar de dagjournaals, een systematiek in het therapeutisch handelen terug te vinden?( Hier wordt bedoeld op de relatie met het behandeldoel en het behandelplan)",
+]
+
+
+def _dossier_toets(antworden: dict, negatief="niet aanwezig", niet_van_toepassing=True):
+
+    mean = Mean()
+    for item in [antworden[q] for q in dossier_toets_q]:
+        if "niet van toepassing" not in item and niet_van_toepassing:
+            mean(negatief not in item)
+    return {"Dossiertoets": mean.mean()}
