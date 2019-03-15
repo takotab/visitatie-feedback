54e509b9589200ef5a09f038e4104547294d40d6
tako_desktop <takotako@gmail.com>
2019-03-15 11:34:52 +0100

    .

--- visitatie/toetsen/twee_meetinstrumenten.py
+++ visitatie/toetsen/twee_meetinstrumenten.py
@@ -8,6 +8,23 @@ def get_meetinstrumenten(form: Form):
     return result
 
 
+meetinstrumenten_q = {
+    "VAS": [
+        "VAS-P gem",
+        "VAS-P gem_end",
+        "VAS-P min",
+        "VAS-P min_end",
+        "VAS-P max",
+        "VAS-P max_end",
+    ],
+    "PSK": ["PSK 1", "PSK 1_end", "PSK 2", "PSK 2_end", "PSK 3", "PSK 3_end"],
+    "NRS": ["NRS", "NRS_end"],
+    "QBPDS": ["QBPDS", "QBPDS_end"],
+    "Anders": ["Welk ander meetinstrument gebruikt u?", "Anders", "Anders_end"],
+}
+# STarT Back Screening Tool,STarT Back Screening Tool_end,GPE1,GPE2
+
+
 def _twee_meetinstrumenten(antworden: dict, negatief="niet aanwezig"):
     return {"twee_meetinstrumenten": 0.25}
 
