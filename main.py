c3b1e44b47e8bdf741eba2e1d477e8c2f951b3c9
tako_desktop <takotako@gmail.com>
2019-03-15 16:13:19 +0100

    .

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def main(map):
     a_form = visitatie.get_data(i=0)
     visitatie.get_colour(a_form)
 
-    # TODO STartBack/GPE
     # TODO Save praktijk_dict as json
     # TODO make pdf rapport
     pass
