854cf925c3e4c011207cc26a90b819d0a4bb7194
tako_desktop <takotako@gmail.com>
2019-03-15 11:33:03 +0100

    checkfile.tested.99%

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -11,6 +11,38 @@ def test_get_data():
     assert a_form.door_de_juiste_bezocht
 
 
-# def test_checkfile():
-#     f = os.path.join("data_fake", "visitatie_form.csv")
-#     f_new = visitatie.get_data.checkfile(f)
+def test_checkfile():
+    f = os.path.join("data_fake", "visitatie_form.csv")
+    f_new = visitatie.get_form_data.checkfile(f)
+    check_in_file(
+        f_new,
+        should_be_s=[
+            "STarT Back Screening Tool,",
+            "PSK 1,PSK 1_end",
+            "PSK 2,PSK 2_end",
+            "PSK 3,PSK 3_end,",
+            "VAS-P gem,VAS-P gem_end,",
+            "NRS,NRS_end,",
+        ],
+        should_not_be_s=[
+            "STarT Back Screening Tool ,",
+            "PSK 1,PSK 1,",
+            "PSK 2,PSK 2,",
+            "PSK 3,PSK 3,",
+            "NRS,NRS,",
+        ],
+    )
+    # f_new2 = visitatie.get_form_data.checkfile(f_new)
+    # assert f_new == f_new2
+    os.remove(f_new)
+
+
+def check_in_file(f: str, should_be_s: [str], should_not_be_s: [str]):
+    with open(f, "r") as f:
+        for line in f:
+            for should_be in should_be_s:
+                assert should_be in line
+            for should_not_be in should_not_be_s:
+                assert should_not_be not in line
+            return
+
--- visitatie/get_form_data.py
+++ visitatie/get_form_data.py
@@ -13,25 +13,47 @@ def get_data(i=0, path="data_fake"):
     return Form(df.iloc[i, :], path)
 
 
-# def checkfile(f: str):
-#     adjusted = False
-#     with open(f, "r") as f:
-#         for line in f:
-#             new_patients = []
-#             patients = line.split("Wilt u nog een patiënt invoeren?,")
-#             for patient in patients:
-#                 patient_qs = []
-#                 for q in patient.split(","):
-#                     if q not in patient_qs:
-#                         patient_qs.append(q)
-#                     else:
-#                         new_q = q + "_end"
-#                         patient_qs.append(new_q)
-#                         adjusted = True
-#                 new_patients.append(",".join(patient_qs))
-#             new_line = "Wilt u nog een patiënt invoeren?,".join(new_patients)
-#             if not adjusted:
-#                 return f
+def checkfile(f_path: str):
+    adjusted = False
+    new_file = ""
+    with open(f_path, "r") as f:
+        first_line = True
+        for line in f:
+            if first_line:
+                new_patients = []
+                patients = line.split("Wilt u nog een patiënt invoeren?,")
+                for patient in patients:
+                    patient_qs = []
+                    for q in patient.split(","):
+                        if "STarT Back Screening Tool " in q:
+                            q = q.replace(
+                                "STarT Back Screening Tool ",
+                                "STarT Back Screening Tool",
+                            )
+                            adjusted = True
+                        if q not in patient_qs:
+                            patient_qs.append(q)
+                        else:
+                            new_q = q + "_end"
+                            patient_qs.append(new_q)
+                            adjusted = True
+                    new_patients.append(",".join(patient_qs))
 
-#             assert len(new_line.split(",")) == len(line.split(","))
-#             print(new_line)
+                new_line = "Wilt u nog een patiënt invoeren?,".join(new_patients)
+
+                if not adjusted:
+                    return f_path
+                assert len(new_line.split(",")) == len(line.split(","))
+                new_file += new_line + os.linesep
+                first_line = False
+            else:
+                new_file += line + os.linesep
+
+    with open(new_file_name(f_path), "w") as f_new:
+        f_new.write(new_file)
+    return new_file_name(f_path)
+
+
+def new_file_name(f: str):
+    return f.replace(".csv", "_adjusted.csv")
+
