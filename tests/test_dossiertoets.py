cc00fbde51b84b041ce34b5e87d952ee6b4c0e9e
tako_desktop <takotako@gmail.com>
2019-03-16 13:16:01 +0100

    .,

--- tests/test_dossiertoets.py
+++ tests/test_dossiertoets.py
@@ -4,7 +4,7 @@ import visitatie
 def test_dossier_toets():
     a_form = visitatie.get_data(i=0)
     _ = visitatie.get_data_from_all_patients(a_form)
-    _dossiertoets = visitatie.toetsen._dossier_toets(a_form.patients[0])
+    _dossiertoets = visitatie.toetsen._dossier_toets(a_form.patients[0], 0)
     assert _dossiertoets == {"Dossiertoets": 0.25}
 
 
@@ -12,7 +12,7 @@ def test_get_dossiertoets():
     a_form = visitatie.get_data(i=0)
     _ = visitatie.get_data_from_all_patients(a_form)
     _dossiertoets = visitatie.toetsen.get_dossier_toets(a_form)
-    assert _dossiertoets["Dossiertoets"] == 0.25
+    assert _dossiertoets["Dossiertoets"] == 0.375
 
 
 def test_get_dossiertoets_2():
