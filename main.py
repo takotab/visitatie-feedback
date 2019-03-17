875df5c4dc2c60ced95e013dee0dc648b7bf871b
tako_desktop <takotako@gmail.com>
2019-03-17 13:34:23 +0100

    now working data. I think. tested.

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ args = parser.parse_args()
 
 def main(dir):
     result = {}
-    for i in range(4, 100):
+    for i in range(4, 66):
         a_form = visitatie.get_data(i=i, path=dir)
-        if a_form.praktijk_code == 9999999:
+        if a_form.praktijk_code == 9_999_999:
             continue
         c = visitatie.get_color(a_form)
         print(i, c)
@@ -27,6 +27,16 @@ def main(dir):
     # TODO Save praktijk_dict as json
     # TODO make pdf rapport
     print(len(result), result)
+    result_r = {}
+    for key, item in result.items():
+        if item in result_r:
+            result_r[item].append(key)
+        else:
+            result_r[item] = [key]
+    for key in result_r:
+        print(f"\n---{key}---\n")
+        print("num of praktijken:", len(result_r[key]))
+        print(result_r[key])
 
 
 if __name__ == "__main__":
